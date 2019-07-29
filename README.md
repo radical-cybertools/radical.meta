ccf435076d79c2c1c1dcf5ad994e753230004179
Andre Merzky <andre@merzky.net>
2019-07-29 12:24:40 +0200

    initial setup

---
+++ README.md
@@ -0,0 +1,14 @@
+
+Package radical.meta
+====================
+
+This Python package has only one purpose, to pull the complete RCT software
+stack as dependencies.
+
+
+License
+-------
+
+This software is released under the
+[LGPL License v3.0](http://opensource.org/licenses/LGPL-3.0).
+
---
+++ src/radical/meta/__init__.py
@@ -0,0 +1,16 @@
+
+# ------------------------------------------------------------------------------
+#
+import os as _os
+import radical.utils as _ru
+
+
+_root = _os.path.dirname(__file__)
+
+version_short, version_detail, version_base, version_branch, \
+        sdist_name, sdist_path = _ru.get_version(_root)
+version = version_short
+
+
+# ------------------------------------------------------------------------------
+
